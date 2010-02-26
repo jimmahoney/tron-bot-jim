6cbe568aa66ed6d4141a59d3854cdfa5e23814a7
James.H.Mahoney <James.H.Mahoney@ff4629a0-1a7c-11df-9a77-830dd48368b2>
2010-02-26 17:38:04 +0000

    maze map; randomizeName option in LogFile

--- trunk/bots/python/utilities.py
+++ trunk/bots/python/utilities.py
@@ -3,7 +3,7 @@
 """
 
 from tron import NORTH, EAST, SOUTH, WEST
-import sys, time
+import sys, time, random
 
 rightwards = {NORTH:EAST, EAST:SOUTH, SOUTH:WEST, WEST:NORTH}
 leftwards = {NORTH:WEST, WEST:SOUTH, SOUTH:EAST, EAST:NORTH}
@@ -11,19 +11,20 @@ vector2direction = {(1,0):SOUTH, (0,1):EAST, (-1,0):NORTH, (0,-1):WEST}
 direction2string = {NORTH:'north', EAST:'east', SOUTH:'south', WEST:'west'}
 
 class LogFile():
-	""" A log file for storing bot messages. 
-            Usage:
-              debug = LogFile("botlog.txt")
-              debug.log("put your message here.")
-        """
-	def __init__(self, name):
-		self.name = name
-		self.file = open(name, 'a')
-		self.file.write("--- '%s' at %s --- \n" % \
-                                  (name, time.asctime()))
-                sys.stderr = self.file    # send errors to the log
-	def log(self, message):
-		self.file.write(message + "\n")
+  """ A log file for storing bot messages. 
+      Usage:
+      debug = LogFile("botlog.txt")
+      debug.log("put your message here.")
+      """
+  def __init__(self, name, randomizeName = False):
+    if randomizeName:
+      name = name + "_" + str(random.randrange(1e6))
+    self.name = name
+    self.file = open(name, 'a')
+    self.file.write("--- '%s' at %s --- \n" % (name, time.asctime()))
+    sys.stderr = self.file       # send errors to the log
+  def log(self, message):
+      self.file.write(message + "\n")
 
 class BotHistory(object):
   """ Keep a record of a bot's history. 
