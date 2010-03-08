037335b31fe1308856f946852fc1cb278c851c83
James.H.Mahoney <James.H.Mahoney@ff4629a0-1a7c-11df-9a77-830dd48368b2>
2010-03-08 22:05:26 +0000

    added flush to log file write

--- bots/python/utilities.py
+++ bots/python/utilities.py
@@ -1,9 +1,15 @@
 """
  Utility classes, functions, and dictionaries for tron bots.
+
+ history: 
+   2010-Mar-8      added log file flushing
+   2010-Feb        original
+ 
+ Jim Mahoney | Feb 2010 | GPL
 """
 
 from tron import NORTH, EAST, SOUTH, WEST
-import sys, time, random
+import sys, time, random, os
 
 rightwards = {NORTH:EAST, EAST:SOUTH, SOUTH:WEST, WEST:NORTH}
 leftwards = {NORTH:WEST, WEST:SOUTH, SOUTH:EAST, EAST:NORTH}
@@ -13,18 +19,22 @@ direction2string = {NORTH:'north', EAST:'east', SOUTH:'south', WEST:'west'}
 class LogFile():
   """ A log file for storing bot messages. 
       Usage:
-      debug = LogFile("botlog.txt")
-      debug.log("put your message here.")
+        debug = LogFile("botlog.txt")
+        debug.log("put your message here.")
       """
-  def __init__(self, name, randomizeName = False):
+  def __init__(self, name, randomizeName = False, append = True):
+    mode = 'a' if append else 'w'  # Append (default) or Write
     if randomizeName:
       name = name + "_" + str(random.randrange(1e6))
+      mode = 'w'                   # Force 'write' mode if random name.
     self.name = name
-    self.file = open(name, 'a')
-    self.file.write("--- '%s' at %s --- \n" % (name, time.asctime()))
-    sys.stderr = self.file       # send errors to the log
+    self.file = open(name, mode)
+    sys.stderr = self.file         # Send runtime errors to this log, too.
+    self.log("--- '%s' at %s --- \n" % (name, time.asctime()))
   def log(self, message):
       self.file.write(message + "\n")
+      self.file.flush()            # Make sure that write finishes now.
+      os.fsync(self.file.fileno()) # Ditto at the operating system level, too.
 
 class BotHistory(object):
   """ Keep a record of a bot's history. 
